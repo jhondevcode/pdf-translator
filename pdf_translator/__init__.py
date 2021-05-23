b454f43e394553ed3b819071178de8bd69f7e12b
Jhon Fernandez <jhondev.code@gmail.com>
2021-05-23 17:52:25 -0500

    I have updated the project metadata path

--- pdf_translator/__init__.py
+++ pdf_translator/__init__.py
@@ -1,7 +1,7 @@
 __name__ = "Pdf Translator"
 __description__ = 'A translation utility for PDF documents'
 __url__ = 'https://github.com/jhondev-code/pdf-translator'
-__version__ = '1.0.0'
+__version__ = '0.1.0'
 __author__ = 'Jhon fernandez'
 __author_email__ = 'jhondev.code@gmail.com'
 __license__ = 'GPL-3.0'
