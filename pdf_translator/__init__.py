2b3e6a1bfd52956cc33a905fdd517f560b3015dd
Jhon Fernandez <jhondev.code@gmail.com>
2021-05-23 19:31:47 -0500

    Metadata update

--- pdf_translator/__init__.py
+++ pdf_translator/__init__.py
@@ -1,8 +1,8 @@
 __name__ = "Pdf Translator"
-__description__ = 'A translation utility for PDF documents'
-__url__ = 'https://github.com/jhondev-code/pdf-translator'
-__version__ = '0.1.0'
-__author__ = 'Jhon fernandez'
-__author_email__ = 'jhondev.code@gmail.com'
-__license__ = 'GPL-3.0'
-__copyright__ = 'Copyright 2021 jhondev-code'
+__description__ = "A translation utility for PDF documents"
+__url__ = "https://github.com/jhondev-code/pdf-translator"
+__version__ = "0.1.0"
+__author__ = "Jhon fernandez"
+__author_email__ = "jhondev.code@gmail.com"
+__license__ = "GPL-3.0"
+__copyright__ = "Copyright 2021 jhondev-code"
