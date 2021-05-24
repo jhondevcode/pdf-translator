4388ca450398f963de5de7bdc6651dfa5d841f3d
Jhon Fernandez <jhondev.code@gmail.com>
2021-05-23 19:28:43 -0500

    Implementations for translators

---
+++ pdf_translator/translation/impl.py
@@ -0,0 +1,9 @@
+"""Implementations"""
+
+
+class Translator:
+    def __init__(self, *args, **kwargs):
+        super(Translator, self).__init__()
+
+    def translate(self, text: str) -> str:
+        pass
